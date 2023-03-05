d0f84b546c8c83f4d5dc6e2d7d2c6ab88c3ae818
lghiggino <lghiggino@gmail.com>
2023-03-05 14:22:26 -0300

    feat: tweet mongoose schema, and save in db test

--- nestjs-tweets/src/tweets/entities/tweet.entity.spec.ts
+++ nestjs-tweets/src/tweets/entities/tweet.entity.spec.ts
@@ -1,13 +1,43 @@
-import { Tweet } from './tweet.entity';
+import mongoose from 'mongoose';
+import { Tweet, TweetSchema } from './tweet.entity';
 
-describe('Tweet', () => {
-  it('should create a tweet', () => {
-    const tweet = new Tweet({
-      content: 'Hello World',
-      screen_name: 'Leonardo',
+describe('Tweets', () => {
+  describe('Tweet Class', () => {
+    it('should create a tweet', () => {
+      const tweet = new Tweet({
+        content: 'Hello World',
+        screen_name: 'Leonardo',
+      });
+
+      expect(tweet.content).toBe('Hello World');
+      expect(tweet.screen_name).toBe('Leonardo');
+    });
+  });
+
+  describe('in Mongo DB', () => {
+    let connection: mongoose.Mongoose;
+    beforeEach(async () => {
+      connection = await mongoose.connect(
+        'mongodb://root:root@localhost:27017/tweets_test?authSource=admin',
+      );
     });
 
-    expect(tweet.content).toBe('Hello World');
-    expect(tweet.screen_name).toBe('Leonardo');
+    afterEach(async () => {
+      await connection.disconnect();
+    });
+
+    it('should create a tweet document', async () => {
+      const TweetModel = connection.model('Tweet', TweetSchema);
+      const tweet = new TweetModel({
+        content: 'Hello World',
+        screen_name: 'Leonardo',
+      });
+
+      await tweet.save();
+      const savedTweet = await TweetModel.findById(tweet._id);
+      expect(savedTweet).toBeDefined();
+      expect(savedTweet.content).toBe('Hello World');
+      expect(savedTweet.screen_name).toBe('Leonardo');
+    });
   });
 });
