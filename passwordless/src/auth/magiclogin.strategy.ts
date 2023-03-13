050794b8f0d569cadca0ffe3c89969e19c83ed8f
lghiggino <lghiggino@gmail.com>
2023-03-13 07:21:09 -0300

    feature: calling email service to send magic login

--- passwordless/src/auth/magiclogin.strategy.ts
+++ passwordless/src/auth/magiclogin.strategy.ts
@@ -1,4 +1,6 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import Strategy from 'passport-magic-login';
 import { AuthService } from './auth.service';
@@ -7,7 +9,11 @@ import { AuthService } from './auth.service';
 export class MagicLoginStrategy extends PassportStrategy(Strategy) {
   private readonly logger = new Logger(MagicLoginStrategy.name);
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private mailService: MailerService,
+    private configService: ConfigService,
+  ) {
     super({
       secret: 'my-secret',
       jwtOptions: {
@@ -19,6 +25,12 @@ export class MagicLoginStrategy extends PassportStrategy(Strategy) {
         this.logger.debug(
           `Sending magic link to ${destination} with link ${href}`,
         );
+        this.mailService.sendMail({
+          to: destination,
+          from: this.configService.get('SENDGRID_MAIL_SENDER'),
+          subject: 'Passwordless Magic Link Login',
+          text: `Sending magic link to ${destination} with link ${href}`,
+        });
       },
       verify: async (payload, callback) => {
         callback(null, this.validate(payload));
