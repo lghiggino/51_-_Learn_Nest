8dea15d41cf00b957d4d4a1c930da39c65a6e719
lghiggino <lghiggino@gmail.com>
2023-03-16 08:11:13 -0300

    chore: added swagger to project

--- nestjs-api-tests/src/app.module.ts
+++ nestjs-api-tests/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
+import { LeadsModule } from './leads/leads.module';
+import { HealthModule } from './health/health.module';
 @Module({
-  imports: [TweetsModule],
+  imports: [TweetsModule, LeadsModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjs-api-tests/src/main.ts
+++ nestjs-api-tests/src/main.ts
@@ -1,8 +1,31 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function buildSwaggerDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('register-client')
+    .setDescription('The client-register project api')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addBasicAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: '*',
+  });
+  buildSwaggerDocumentation(app);
   await app.listen(process.env.PORT);
 }
 bootstrap();
