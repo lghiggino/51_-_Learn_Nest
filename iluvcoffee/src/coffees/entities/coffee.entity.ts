9efd9da91be27a68d3ad7d28875dab0d4c40e03e
lghiggino <lghiggino@gmail.com>
2023-04-01 19:31:54 -0300

    feature: added typeorm, TypeOrmModule @ app.module, and created TORM @Entity

--- iluvcoffee/src/coffees/entities/coffee.entity.ts
+++ iluvcoffee/src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
+
+  // @CreateDateColumn()
+  // createdAt: Date;
+
+  // @UpdateDateColumn()
+  // updatedAt: Date;
+
+  // @DeleteDateColumn()
+  // deletedAt: Date;
 }
